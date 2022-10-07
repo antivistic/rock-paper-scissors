0b694ec83370f8f2c87801290d491a752c817d80
antivistic <schluek@gmail.com>
2022-10-07 14:35:17 -0400

    updated index to show only a begin button // updated js to play a single round on "begin" button click and display win, lose, or tie. // added error handling on player prompt.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,5 @@
+let playerSelection;
+
 function getComputerChoice() {
   let randNum = Math.random();
   let result;
@@ -11,18 +13,25 @@ function getComputerChoice() {
   return result;
 }
 
+function getPlayerChoice() {
+	playerSelection = prompt("Rock, Paper, or Scissors?");
+	return playerSelection.toLowerCase();
+}
+
 function singleRound(playerSelection, computerSelection) {
 
     if ((playerSelection === "rock" && computerSelection === "scissors")
          || (playerSelection === "paper" && computerSelection === "rock")
          || (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-    	alert("You Win!!");
+    	    alert("You Win!!");
       } else if ((playerSelection === "rock" && computerSelection === "paper")
          || (playerSelection === "paper" && computerSelection === "scissors")
          || (playerSelection === "scissors" && computerSelection === "rock")) {
          	alert("You Lose!!");
+         } else if ((playerSelection === computerSelection)) {
+         	alert("TIE!!!");
          } else {
-         	alert ("TIE!!!");
+         	prompt("Please try again and enter only rock, paper, or scissors (NOT case-sensative.)");
          }
 }
